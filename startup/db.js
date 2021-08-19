53fd41e5f06ccb168368d15f220d79333db3da44
Akhad  Yulchiev <55797133+akhad7799@users.noreply.github.com>
2021-08-19 11:40:56 +0500

    chang on db

--- startup/db.js
+++ startup/db.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 
+//export connection to mongodb
 module.exports = function () {
     mongoose.connect('mongodb://localhost/shop', {useNewUrlParser: true, useUnifiedTopology: true})
         .then(()=> {
